adb0e45ec54f116410ccf63ae630746ce05457df
Mohammmedrafique <pushpendrasharmakarauli@gmail.com>
2024-03-28 12:11:23 +0530

    d

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const connection = require("./db");
-
+app.use(cors({ origin: 'http://localhost:3000' }));
 const { userRouter } = require("./routes/user.routes");
 const { noteRouter } = require("./routes/note.routes");
 
